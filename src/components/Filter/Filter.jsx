cb1e1781189fc5e2cb839c026ce25f70757bdd89
dreamogu <dreamogu59@gmail.com>
2023-05-17 13:06:15 +0900

    li 태그를 button로 수정

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -7,59 +7,57 @@ export default function Filter({ filter, setFilter }) {
   };
 
   return (
-    <section className={styles.filter_container}>
-      <ul className={styles.filter_item}>
-        <li
-          onClick={() => filterHandler('total')}
-          className={filter === 'total' ? styles.selected : ''}
-        >
-          <img
-            src='/img/total.png'
-            alt=''
-          />
-          <span className={styles.type}>전체</span>
-        </li>
-        <li
-          onClick={() => filterHandler('product')}
-          className={filter === 'product' ? styles.selected : ''}
-        >
-          <img
-            src='/img/product.png'
-            alt=''
-          />
-          <span className={styles.type}>상품</span>
-        </li>
-        <li
-          onClick={() => filterHandler('category')}
-          className={filter === 'category' ? styles.selected : ''}
-        >
-          <img
-            src='/img/category.png'
-            alt=''
-          />
-          <span className={styles.type}>카테고리</span>
-        </li>
-        <li
-          onClick={() => filterHandler('exhibition')}
-          className={filter === 'exhibition' ? styles.selected : ''}
-        >
-          <img
-            src='/img/exhibition.png'
-            alt=''
-          />
-          <span className={styles.type}>기획전</span>
-        </li>
-        <li
-          onClick={() => filterHandler('brand')}
-          className={filter === 'brand' ? styles.selected : ''}
-        >
-          <img
-            src='/img/brand.png'
-            alt=''
-          />
-          <span className={styles.type}>브랜드</span>
-        </li>
-      </ul>
+    <section className={styles.filter_item}>
+      <button
+        onClick={() => filterHandler('total')}
+        className={filter === 'total' ? styles.selected : ''}
+      >
+        <img
+          src='/img/total.png'
+          alt=''
+        />
+        <span className={styles.type}>전체</span>
+      </button>
+      <button
+        onClick={() => filterHandler('product')}
+        className={filter === 'product' ? styles.selected : ''}
+      >
+        <img
+          src='/img/product.png'
+          alt=''
+        />
+        <span className={styles.type}>상품</span>
+      </button>
+      <button
+        onClick={() => filterHandler('category')}
+        className={filter === 'category' ? styles.selected : ''}
+      >
+        <img
+          src='/img/category.png'
+          alt=''
+        />
+        <span className={styles.type}>카테고리</span>
+      </button>
+      <button
+        onClick={() => filterHandler('exhibition')}
+        className={filter === 'exhibition' ? styles.selected : ''}
+      >
+        <img
+          src='/img/exhibition.png'
+          alt=''
+        />
+        <span className={styles.type}>기획전</span>
+      </button>
+      <button
+        onClick={() => filterHandler('brand')}
+        className={filter === 'brand' ? styles.selected : ''}
+      >
+        <img
+          src='/img/brand.png'
+          alt=''
+        />
+        <span className={styles.type}>브랜드</span>
+      </button>
     </section>
   );
 }
