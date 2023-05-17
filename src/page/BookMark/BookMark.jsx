d7a1c07ab753066fe4bcaedab5720b43e8c8589b
dreamogu <dreamogu59@gmail.com>
2023-05-17 12:32:30 +0900

    무한 스크롤 구현

--- src/page/BookMark/BookMark.jsx
+++ src/page/BookMark/BookMark.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from '../../components/Filter/Filter';
 import { useSelector } from 'react-redux';
 import styles from './BookMark.module.css';
 import Product from '../../components/Product/Product';
+import { useInView } from 'react-intersection-observer';
 
 export default function BookMark() {
   const bookmarks = useSelector((state) => state.bookmark.isBookmarked);
+  const [page, setPage] = useState(1);
+  const { ref, inView } = useInView({ triggerOnce: false });
   const [filter, setFilter] = useState('total');
 
   const filteredbookmarks = bookmarks.filter((bookmark) => {
@@ -17,6 +20,15 @@ export default function BookMark() {
     return false;
   });
 
+  const reverseFilteredBookmarks = filteredbookmarks.reverse();
+  const bookmarkToShow = reverseFilteredBookmarks.slice(0, page * 12);
+  useEffect(() => {
+    if (inView) setPage((prevPage) => prevPage + 1);
+  }, [inView]);
+  useEffect(() => {
+    setPage(1);
+  }, [filter]);
+
   return (
     <section className={styles.container}>
       <Filter
@@ -24,14 +36,17 @@ export default function BookMark() {
         setFilter={setFilter}
       />
       {bookmarks ? (
-        <ul className={styles.ul}>
-          {filteredbookmarks.reverse().map((bookmark) => (
-            <Product
-              key={bookmark.id}
-              {...bookmark}
-            />
-          ))}
-        </ul>
+        <>
+          <ul className={styles.ul}>
+            {bookmarkToShow.map((bookmark) => (
+              <Product
+                key={bookmark.id}
+                {...bookmark}
+              />
+            ))}
+          </ul>
+          <div ref={ref} />
+        </>
       ) : (
         <div>북마크가 없습니다.</div>
       )}
--- src/page/Products/ProductsList.jsx
+++ src/page/Products/ProductsList.jsx
@@ -1,15 +1,70 @@
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchProducts } from '../../redux/productSlice';
+// import Product from '../../components/Product/Product';
+// import styles from './ProductsList.module.css';
+// import Filter from '../../components/Filter/Filter';
+
+// function ProductsList() {
+//   const dispatch = useDispatch();
+//   const products = useSelector((state) => state.products.items);
+//   const loading = useSelector((state) => state.products.loading);
+//   const [filter, setFilter] = useState('total');
+//   const filteredProducts = products.filter((product) => {
+//     if (filter === 'total') return true;
+//     if (filter === 'product') return product.type === 'Product';
+//     if (filter === 'category') return product.type === 'Category';
+//     if (filter === 'exhibition') return product.type === 'Exhibition';
+//     if (filter === 'brand') return product.type === 'Brand';
+//     return false;
+//   });
+
+//   useEffect(() => {
+//     dispatch(fetchProducts(null));
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <section className={styles.container}>
+//         <Filter
+//           filter={filter}
+//           setFilter={setFilter}
+//         />
+//         {loading ? (
+//           <div>Loading...</div>
+//         ) : (
+//           <ul className={styles.ul}>
+//             {filteredProducts.map((product) => (
+//               <Product
+//                 key={product.id}
+//                 {...product}
+//               />
+//             ))}
+//           </ul>
+//         )}
+//       </section>
+//     </>
+//   );
+// }
+
+// export default ProductsList;
+
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../../redux/productSlice';
 import Product from '../../components/Product/Product';
 import styles from './ProductsList.module.css';
 import Filter from '../../components/Filter/Filter';
+import { useInView } from 'react-intersection-observer';
 
 function ProductsList() {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.items);
   const loading = useSelector((state) => state.products.loading);
+  const [page, setPage] = useState(1);
+  const { ref, inView } = useInView({ triggerOnce: false });
   const [filter, setFilter] = useState('total');
+
   const filteredProducts = products.filter((product) => {
     if (filter === 'total') return true;
     if (filter === 'product') return product.type === 'Product';
@@ -19,9 +74,19 @@ function ProductsList() {
     return false;
   });
 
+  const productsToShow = filteredProducts.slice(0, page * 12);
+
   useEffect(() => {
     dispatch(fetchProducts(null));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (inView) setPage((prevPage) => prevPage + 1);
+  }, [inView]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [filter]);
 
   return (
     <>
@@ -33,14 +98,17 @@ function ProductsList() {
         {loading ? (
           <div>Loading...</div>
         ) : (
-          <ul className={styles.ul}>
-            {filteredProducts.map((product) => (
-              <Product
-                key={product.id}
-                {...product}
-              />
-            ))}
-          </ul>
+          <>
+            <ul className={styles.ul}>
+              {productsToShow.map((product) => (
+                <Product
+                  key={product.id}
+                  {...product}
+                />
+              ))}
+            </ul>
+            <div ref={ref} />
+          </>
         )}
       </section>
     </>
